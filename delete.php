ecbe9faf1d1becc0d650ed65c45b3a1372ab667d
tech-asa <tech-asai07@gmail.com>
2022-09-12 16:58:54 +0900

    レシピ削除機能実装

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+    $user = "mysql";
+    $pass = "mysql";
+
+    if(empty($_GET["id"])){
+        echo "IDを正しく入力してください";
+        exit;
+    }
+
+    try {
+        $id = (int)$_GET["id"];
+        $dbh = new PDO("mysql:host = localhost; dbname=db1; charset=utf8", $user, $pass);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE FROM recipes WHERE id = ?";
+        $stml = $dbh->prepare($sql);
+        $stml->bindValue(1, $id, PDO::PARAM_INT);
+        $stml->execute();
+        $dbh = null;
+        echo "ID:" . htmlspecialchars($id, ENT_QUOTES) . "の削除が完了しました。";
+    } catch (PDOException $e) {
+        echo "エラー発生：". htmlspecialchars($e->getMessage(), ENT_QUOTES)."<br>";
+        exit;
+    }
+?>
